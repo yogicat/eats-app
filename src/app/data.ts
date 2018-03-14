813769c7011dedcff79ab0506747f1cf9e81f3b8
elainena <elainena93@gmail.com>
2018-03-14 15:24:29 +0900

    Add Menu Data

---
+++ src/app/data.ts
@@ -0,0 +1,58 @@
+let menu: [
+  {
+    id: 1;
+    name: "Green Tea with Salted Cheese";
+    category: "salted cheese";
+    contents: "";
+    price: 5.25;
+  },
+  {
+    id: 2;
+    name: "Black Tea with Salted Cheese";
+    category: "salted cheese";
+    contents: "";
+    price: 5.25;
+  },
+  {
+    id: 3;
+    name: "Chocolate with Salted Cheese";
+    category: "tea series";
+    contents: "";
+    price: 5.35;
+  },
+  {
+    id: 4;
+    name: "Milk Tea with Oreo and Tiramisu Salted Cheese";
+    category: "milk tea";
+    contents: "";
+    price: 5.4;
+  },
+  {
+    id: 5;
+    name: "Chocolate with Tiramisu Salted Cheese";
+    category: "milk tea";
+    contents: "";
+    price: 5.5;
+  },
+  {
+    id: 6;
+    name: "Chocolate with Tiramisu Salted Cheese";
+    category: "tea series";
+    contents: "";
+    price: 5.5;
+  },
+  {
+    id: 7;
+    name: "Matcha Latte with Tiramisu Salted Cheese";
+    category: "salted cheese";
+    contents: "";
+    price: 5.5;
+  },
+  {
+    id: 8;
+    name: "Light Oolong Tea with Salted Cheese";
+    category: "milk tea";
+    contents: "";
+    price: 5.5;
+  }
+];
