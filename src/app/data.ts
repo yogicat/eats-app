49e019b99fb32c465d3afc28a3e9855611f4d5fb
elainena <elainena93@gmail.com>
2018-03-14 15:28:36 +0900

    Edit quotation marks

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,30 +1,30 @@
 let menu: [
   {
     id: 1;
-    name: "Green Tea with Salted Cheese";
-    category: "salted cheese";
-    contents: "";
+    name: 'Green Tea with Salted Cheese';
+    category: 'salted cheese';
+    contents: '';
     price: 5.25;
   },
   {
     id: 2;
-    name: "Black Tea with Salted Cheese";
-    category: "salted cheese";
-    contents: "";
+    name: 'Black Tea with Salted Cheese';
+    category: 'salted cheese';
+    contents: '';
     price: 5.25;
   },
   {
     id: 3;
-    name: "Chocolate with Salted Cheese";
-    category: "tea series";
-    contents: "";
+    name: 'Chocolate with Salted Cheese';
+    category: 'tea series';
+    contents: '';
     price: 5.35;
   },
   {
     id: 4;
-    name: "Milk Tea with Oreo and Tiramisu Salted Cheese";
-    category: "milk tea";
-    contents: "";
+    name: 'Milk Tea with Oreo and Tiramisu Salted Cheese';
+    category: 'milk tea';
+    contents: '';
     price: 5.4;
   },
   {
