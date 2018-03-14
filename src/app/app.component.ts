1932be96803ab630e6c5ea7ebff2907b313f3dc2
yogicat <dahe.oh@gmail.com>
2018-03-14 15:47:41 +0900

    edit files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+import { Imenu } from './data';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,62 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  menu: Imenu[] = [
+      {
+        id: 1,
+        name: 'Green Tea with Salted Cheese',
+        category: 'salted cheese',
+        contents: '',
+        price: 5.25
+      },
+      {
+        id: 2,
+        name: 'Black Tea with Salted Cheese',
+        category: 'salted cheese',
+        contents: '',
+        price: 5.25
+      },
+      {
+        id: 3,
+        name: 'Chocolate with Salted Cheese',
+        category: 'tea series',
+        contents: '',
+        price: 5.35
+      },
+      {
+        id: 4,
+        name: 'Milk Tea with Oreo and Tiramisu Salted Cheese',
+        category: 'milk tea',
+        contents: '',
+        price: 5.4
+      },
+      {
+        id: 5,
+        name: 'Chocolate with Tiramisu Salted Cheese',
+        category: 'milk tea',
+        contents: '',
+        price: 5.5
+      },
+      {
+        id: 6,
+        name: 'Chocolate with Tiramisu Salted Cheese',
+        category: 'tea series',
+        contents: '',
+        price: 5.5
+      },
+      {
+        id: 7,
+        name: 'Matcha Latte with Tiramisu Salted Cheese',
+        category: 'salted cheese',
+        contents: '',
+        price: 5.5
+      },
+      {
+        id: 8,
+        name: 'Light Oolong Tea with Salted Cheese',
+        category: 'milk tea',
+        contents: '',
+        price: 5.5
+      }
+  ];
 }
